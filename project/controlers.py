af34a372429b18e07372d776d1edf345040ea723
rabcanj <rabcanj@gmail.com>
2019-09-11 17:35:20 -0400

    asdasd

--- project/controlers.py
+++ project/controlers.py
@@ -1,7 +1,18 @@
 from app import app
+import sqlalchemy as db
 from flask import render_template
+
+
+def get_connection():
+    engine = db.create_engine('sqlite:///census.sqlite')
+    return engine.connect()
 
 
 @app.route('/index')
 def index():
+    connection = get_connection()
+    result = connection.execute("select * from CATEGORY")
+    for row in result:
+        print("username:", row['name'])
+    connection.close()
     return render_template('index.html', user={})
